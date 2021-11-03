e0e9bb248676bdaa2868e8f0cbe9087a73182d2b
davidjwp <davidpwjacobs@gmail.com>
2021-11-03 23:12:32 +0100

    big funny house haha lol

---
+++ pitite_maison/house.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+int main()
+{
+    printf("          \"\n");
+    printf("        @ @ \" @\n");
+    printf("       []___\\\n");
+    printf("      /    /\\___\n");
+    printf("(~)  /_/\\_//____/\\\n");
+    printf(" |   | || |||__|||\n");
+    return 0;
+}
