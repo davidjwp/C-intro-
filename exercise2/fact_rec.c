640b2d79d922223c462aa2e0946676caa3186c22
davidjwp <davidpwjacobs@gmail.com>
2021-11-14 19:05:14 +0100

    factorielle recursif

--- exercise2/fact_rec.c
+++ exercise2/fact_rec.c
@@ -3,18 +3,14 @@
 
 unsigned long fact_recursion(unsigned n)
 {
-        unsigned long i = i * n; 
+        if (n == 1)
+                return 1;
+        return fact_recursion(n-1) * n;
 
 }
 int main (int argc, char *argv[])
 {
+        printf("%ld\n",fact_recursion(atoi(argv[1])));
 
-        unsigned long i = 1;
-                while (i < n)
-                {
-                        i++;                
-                        printf("%ld\n",fact_recursion(atoi(argv[1])));
-                }
         return(0);
-
 }
