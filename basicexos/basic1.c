31d0dbe60d6a23d5ca8338a6cc5d1bd598745b46
davidjwp <davidpwjacobs@gmail.com>
2021-12-18 15:23:04 +0100

    simple basic c exos

---
+++ basicexos/basic1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+        //char array declaration storing script 
+
+        char Name[]     = "david jacobs";
+        char DOB[]      = "19/04/1997";
+        char Mobile[]   = "06 48 23 31 39";
+
+        //printf char array 
+
+        printf("Name    : %s\n",Name);
+        printf("DOB     : %s\n",DOB);
+        printf("Mobile  : %s\n",Mobile);
+        return(0);
+}
---
+++ basicexos/basic2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+        printf("the C version is %ld\n",__STDC_VERSION__);
+        return(0);
+}
