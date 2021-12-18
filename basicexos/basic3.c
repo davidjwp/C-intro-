9b772614dade5e171b4c960d387b2d179e390a64
davidjwp <davidpwjacobs@gmail.com>
2021-12-18 23:45:37 +0100

    exo3

---
+++ basicexos/basic3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main ()
+{
+        //printf F
+
+        printf("#####\n");
+        printf("#    \n");
+        printf("##   \n");
+        printf("#    \n");
+        printf("#    \n");
+        printf("#    \n");
+
+        //printf C
+
+        printf("  ## \n");
+        printf(" #  #\n");
+        printf("#    \n");
+        printf("#    \n");
+        printf(" #   \n");
+        printf("  ## \n");
+
+        return(0);
+}
